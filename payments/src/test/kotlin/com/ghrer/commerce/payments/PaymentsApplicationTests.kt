186bd45ba3287e24d5de5cbc21d32dc105b0dca4
Hussein Ghrer <hussin.ghrer@gmail.com>
2024-05-31 13:03:14 +0200

    Initial commit. Initialize orders and payments services. Create docker-compose file for the project

---
+++ payments/src/test/kotlin/com/ghrer/commerce/payments/PaymentsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ghrer.commerce.payments
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
